6634348e3f5235948e3a432a1cbbddadf38960bf
en4x-krb <ajaykrgb@gmail.com>
2017-05-09 11:42:25 +0530

    Added Comments

--- 1434_SE_LabTest1/src/Car.java
+++ 1434_SE_LabTest1/src/Car.java
@@ -2,10 +2,12 @@
 public class Car {
 	private int EngineCC;
 
+	//Constructor to initialize engine capacity of the car
 	public Car(int cc) {
 		this.EngineCC = cc;
 	}
 
+	//Method to get car's engine capacity
 	public int getEngineCapacity() {
 		return this.EngineCC;
 	}
